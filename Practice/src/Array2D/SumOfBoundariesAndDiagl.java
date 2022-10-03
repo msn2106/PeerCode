da43f7c21a2466326bcb7ddc4a6c89fee86d3651
Deepak Gupta <deepakgupta71628@gmail.com>
2022-10-03 17:12:23 +0530

    k sorted fixed

---
+++ Practice/src/Array2D/SumOfBoundariesAndDiagl.java
@@ -0,0 +1,10 @@
+package Array2D;
+
+public class SumOfBoundariesAndDiagl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Practice/src/ReturnK_RotatedArray.java
+++ Practice/src/ReturnK_RotatedArray.java
@@ -7,7 +7,8 @@ public class ReturnK_RotatedArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(checkRotation(new int[] {5 ,6 ,7 ,8 ,9 ,10})+1);
+		System.out.println(checkRotation(new int[] {12 ,17 ,1 ,3 ,6 ,11}));
+		System.out.println(checkRotation(new int[] {1,2,3,4,5,6,7}));
 	}
 	
 public static int checkRotation(int[]arr) {
@@ -18,7 +19,8 @@ public static int checkRotation(int[]arr) {
 		while(min<=max) {
 			int mid=min+(max-min)/2;
 			
-			if(mid<arr.length-1 && arr[mid]>arr[mid+1]) return mid;
+			if(mid<max && arr[mid]>arr[mid+1]) return mid+1;
+			if(mid>min && arr[mid]<arr[mid-1]) return mid;
 			
 			if(arr[mid]>arr[min]) {
 				min=mid+1;
@@ -29,9 +31,9 @@ public static int checkRotation(int[]arr) {
 			
 		}
 		
-		if(min==arr.length-1) return -1;
+		if(min==arr.length-1) return 0;
 		
-		return min;
+		return min+1;
 		
 	}
 
